e0e1a840c839c7cdd44520568165271d75f45b8a
Mecharis <121699952+mecharis-420@users.noreply.github.com>
2023-12-21 21:15:24 -0500

    drag games in to public pls ty

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const createBareServer = require("@tomphttp/bare-server-node");
+const path = require("path");
+const http = require("http");
+
+const __dirname = process.cwd();
+const server = http.createServer();
+const app = express();
+const bareServer = createBareServer("/bare/");
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "static")));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
+});
+
+server.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+   (req, res);
+  }
+});
+
+server.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+server.on("listening", () => {
+  console.log(`GameHub running on port 8080`);
+});
+
+server.listen({
+  port: 8080,
+});
